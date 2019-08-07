8d1fc28362d1cb1d8a35f1b0d3c90bc60710eb7a
akasherali <53849485+akasherali@users.noreply.github.com>
2019-08-07 09:50:21 +0500

    Create goto.js

---
+++ goto.js
@@ -0,0 +1,37 @@
+	function gotogoldpills(productId) {
+		document.location.href='https://www.qualitymedstoday.com/#pamparam/';
+	}
+	function gototerapharm(productId) {
+		document.location.href='http://cheap-pharma.com/' + productId +'?id=1401';
+	}
+		function gotoseostats(productId) {
+		document.location.href='http://www.live-drugstore.com/product/' + productId + '?acc=5730';
+	}
+	function gotoMedspremium(productId) {
+		document.location.href='https://www.qualitymedstoday.com/index.php/' + productId + '#pamparam';
+	}	
+	function gotopharmadiscount(productId) {
+		document.location.href='https://www.qualitymedstoday.com/index.php/' + productId + '#pamparam';
+ 	}
+	function gotogo(productId) {
+                document.location.href='http://www.besttabcenter.com/' + productId;
+        }
+		
+	function gotoShop5(acc5) {
+		window.open('http://cheap-pharma.com/categories/Bestsellers/Viagra/?id=1401');
+	}
+	function gotoShop6(acc6) {
+		window.open('http://cheap-pharma.com/categories/Bestsellers/Cialis/?id=1401');
+	}
+	function gotoShop7(acc7) {
+		window.open('http://cheap-pharma.com/categories/Bestsellers/Kamagra/?id=1401');
+	}
+	function gotoShop8(acc8) {
+		window.open('http://cheap-pharma.com/categories/Bestsellers/Levitra/?id=1401');
+	}
+	function gotoShop9(acc8) {
+		window.open('http://cheap-pharma.com/categories/Other/Neurontin/?id=1401');
+	}
+        function gotogoldpillsDEFUNCT(productId) {
+		document.location.href='http://yourmedicinesrxxx.com/' + productId;
+        }
