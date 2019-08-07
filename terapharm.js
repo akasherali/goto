d72529946c209d2fd2aefb2f69c397609821932c
akasherali <53849485+akasherali@users.noreply.github.com>
2019-08-07 09:52:27 +0500

    Create terapharm.js

---
+++ terapharm.js
@@ -0,0 +1,44 @@
+document.write('<table border="0" cellpadding="0" class="table" cellspacing="0" style="margin: 0px;width: 515px;">\
+   <tbody>\
+   <tr>\
+	   <td style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" class="able">Pharmacy:</td> \
+	   <td style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" ><img src="http://nschoose.com/images/charmpharm.png" alt="" ></td> \
+   </tr>\
+   <tr> \
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" class="able">Drug Name:</td>\
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" >Strattera</td>\
+   </tr>\
+   <tr>\
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" class="able">Tablet Strength:</td>\
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" >10 mg, 18mg, 25 mg, 40 mg, 60 mg</td>\
+   </tr>\
+   <tr> \
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" class="able">Available Packages:</td>\
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" >30, 60, 90, 120, 180, 270, 360 pills</td>\
+   </tr>\
+   <tr>\
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" class="able">Best Price:</td>  \
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" > <a href="#buy-strattera" onclick="return gototerapharm(\'categories/Other/Strattera\');">$0.97 Per Tube</a></td> \
+   </tr>\
+   <tr>\
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" class="able">Bestseller:</td>  \
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" ><a href="#buy-strattera" onclick="return gototerapharm(\'categories/Other/Strattera\');">90 mg * 25 mg - $87.72</a></td>   \
+   </tr> 	\
+   <tr>       \
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" class="able">Payment:</td>    \
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" ><b>Visa</b>, <b>MasterCard</td>   \
+   </tr> 					\
+   <tr>                      \
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" class="able">Shipment:<br></td>       \
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" >Free Shipping</td>        \
+   </tr> 					\
+   <tr>                     \
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" class="able">RX:</td>  \
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" >Not Needed</td>    \
+   </tr> 			\
+   <tr>                \
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" class="able">How to Buy?</td>  \
+	   <td  style="text-align: center;font-weight: bold;font-size: 12px;padding: 7px;" > <a href="#buy-strattera" onclick="return gototerapharm(\'categories/Other/Strattera\');"><img src="images/b.gif" alt="" style="border: medium none;"></a></td> \
+   </tr>            \
+   </tbody>\
+   </table>');
